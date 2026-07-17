11abf3999d879230a963b1cad1451c3173ca909d
LeSisyphus <115610385+LeSisyphus@users.noreply.github.com>
2026-07-18 01:49:50 +0800

    refactor: use cursor pagination for transactions

--- lib/features/transaction/domain/repositories/transaction_repository.dart
+++ lib/features/transaction/domain/repositories/transaction_repository.dart
@@ -1,12 +1,19 @@
 import 'package:kasentra/features/transaction/domain/entities/transaction.dart';
 import 'package:kasentra/features/transaction/domain/entities/transaction_item.dart';
+import 'package:kasentra/features/transaction/domain/entities/transaction_page.dart';
 
 abstract class TransactionRepository {
-  Stream<List<Transaction>> watchTransactions({
+  /// Mengambil histori transaksi menggunakan keyset pagination.
+  ///
+  /// Urutan wajib:
+  /// transactionDate descending, lalu transactionId descending.
+  Future<TransactionPage> getTransactionsPage({
     required String businessId,
     DateTime? startDate,
     DateTime? endDate,
     TransactionType? type,
+    required int limit,
+    TransactionCursor? cursor,
   });
 
   Future<Transaction?> getTransactionById({
@@ -20,7 +27,6 @@ abstract class TransactionRepository {
 
   /// Menyimpan transaksi pengeluaran.
   ///
-  /// Penjualan tidak boleh disimpan melalui method ini.
   /// Penjualan harus menggunakan SaleRepository.recordSale()
   /// agar transaksi, item, stok, dan piutang diproses secara atomik.
   Future<void> saveExpense(Transaction expense);
