9dbaef8719b5770a87d0ed8128c1935f8b037951
LeSisyphus <115610385+LeSisyphus@users.noreply.github.com>
2026-07-19 05:29:36 +0800

    chore(app): set up Riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'app/app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const KasentraApp());
+  runApp(const ProviderScope(child: KasentraApp()));
 }
